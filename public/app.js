e3e7718a132c9de505eb1075ca7c61160d7c0289
Ross Crichton <rcrichton87@googlemail.com>
2017-04-10 15:10:29 +0100

    can add one cat programatically

--- public/app.js
+++ public/app.js
@@ -6,8 +6,22 @@ cat.classList.add("cat");
 var name = document.createElement("li");
 name.innerText = "Name: Amber";
 
-var favouriteFood;
-var image;
+var favouriteFood = document.createElement("li");
+favouriteFood.innerText = "Favourite Food: Chicken";
+
+var imageLi = document.createElement("li");
+imageLi.classList.add("image");
+var image = document.createElement("img");
+image.src = "http://i.imgur.com/mtj8mFV.jpg"
+image.style.width = "500px";
+imageLi.appendChild(image);
+
+cat.appendChild(name);
+cat.appendChild(favouriteFood);
+cat.appendChild(imageLi);
+
+var cats = document.querySelector("#cats");
+cats.appendChild(cat);
 
 
 }

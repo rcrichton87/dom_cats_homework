d17bc540be662a265902e56b17209b198f8f80c4
Ross Crichton <rcrichton87@googlemail.com>
2017-04-10 15:21:32 +0100

    split adding a cat into multiple functions

--- public/app.js
+++ public/app.js
@@ -1,27 +1,77 @@
+var createCat = function(){
+  var cat = document.createElement("ul");
+  cat.classList.add("cat");
+  return cat;
+}
+
+var createName = function(inputName){
+  var name = document.createElement("li");
+  name.innerText = "Name: " + inputName;
+  return name;
+}
+
+var createFavouriteFood = function(favourite){
+  var favouriteFood = document.createElement("li");
+  favouriteFood.innerText = "Favourite Food: " + favourite;
+  return favouriteFood;
+}
+
+var createImageLi = function(imageURL){
+  var imageLi = document.createElement("li");
+  imageLi.classList.add("image");
+  var image = document.createElement("img");
+  image.src = imageURL;
+  image.style.width = "500px";
+  imageLi.appendChild(image);
+  return imageLi;
+}
+
+var appendElements = function(cat, name, favouriteFood, imageLi){
+  cat.appendChild(name);
+  cat.appendChild(favouriteFood);
+  cat.appendChild(imageLi);
+
+  var cats = document.querySelector("#cats");
+  cats.appendChild(cat);
+}
+
+var addCat = function(name, favouriteFood, imageURL){
+  var cat = createCat();
+  var catName = createName(name);
+  var catFavouriteFood = createFavouriteFood(favouriteFood);
+  var catImageLi = createImageLi(imageURL);
+
+  appendElements(cat, catName, catFavouriteFood, catImageLi);
+}
+
+
+
 var app = function(){
 
-var cat = document.createElement("ul");
-cat.classList.add("cat");
+// var cat = document.createElement("ul");
+// cat.classList.add("cat");
 
-var name = document.createElement("li");
-name.innerText = "Name: Amber";
+// var name = document.createElement("li");
+// name.innerText = "Name: Amber";
 
-var favouriteFood = document.createElement("li");
-favouriteFood.innerText = "Favourite Food: Chicken";
+// var favouriteFood = document.createElement("li");
+// favouriteFood.innerText = "Favourite Food: Chicken";
 
-var imageLi = document.createElement("li");
-imageLi.classList.add("image");
-var image = document.createElement("img");
-image.src = "http://i.imgur.com/mtj8mFV.jpg"
-image.style.width = "500px";
-imageLi.appendChild(image);
+// var imageLi = document.createElement("li");
+// imageLi.classList.add("image");
+// var image = document.createElement("img");
+// image.src = "http://i.imgur.com/mtj8mFV.jpg"
+// image.style.width = "500px";
+// imageLi.appendChild(image);
 
-cat.appendChild(name);
-cat.appendChild(favouriteFood);
-cat.appendChild(imageLi);
+// cat.appendChild(name);
+// cat.appendChild(favouriteFood);
+// cat.appendChild(imageLi);
 
-var cats = document.querySelector("#cats");
-cats.appendChild(cat);
+// var cats = document.querySelector("#cats");
+// cats.appendChild(cat);
+
+addCat("Amber", "Chicken", "http://i.imgur.com/mtj8mFV.jpg");
 
 
 }
